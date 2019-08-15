10136984cb2257ef0719435687e93c51796e0503
kumarg160491 <48169382+kumarg160491@users.noreply.github.com>
2019-08-15 15:19:16 +0530

    first commit

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -11,7 +11,7 @@ y=dataset.iloc[:,2].values
 
 #Train and test dataset
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x_train,y_train,random_selection=0)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_selection=0)
 
 
 #linear regression cassifier
