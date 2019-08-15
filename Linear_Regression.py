e3289fd156164c7cb8e5037d30617c8c50a056f7
kumarg160491 <48169382+kumarg160491@users.noreply.github.com>
2019-08-15 15:25:14 +0530

    fix again

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,20 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 22:36:51 2019
+
+@author: rajkumar
+"""
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-dataset=pd.read_csv('E:\\data science\\subject vedios\\Polynomial-Linear-Regression-master\\Polynomial-Linear-Regression-master\\Position_Salaries.csv')
+dataset=pd.read_csv('E:\\data science\\subject vedios\\simple-Linear-Regression-master\\simple-Linear-Regression-master\\Salary_Data.csv')
 dataset
 
-x=dataset.iloc[:,1:2].values
-y=dataset.iloc[:,2].values
+x=dataset.iloc[:,:-1]
+x
+y=dataset.iloc[:,1]
+y
 
-#Train and test dataset
+#train and test
+
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_selection=0)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_state=0)
 
 
-#linear regression cassifier
+#regression classifier
+
 from sklearn.linear_model import LinearRegression
-linear_reg1=LinearRegression()
-linear_reg1.fit(x,y)
+lr=LinearRegression()
+lr.fit(x_train,y_train)
+
+y_prid=lr.predict(x_test)
+y_prid
+
+
+#visualising the training dataset
+plt.scatter(x_train,y_train,color='red')
+plt.plot(x_train,lr.predict(x_train),color='blue')
+
